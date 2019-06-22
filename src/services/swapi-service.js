95946ba109c0aa72a9d94a3a3968a46dfec53239
RukkiesMan <pavelekseev@gmail.com>
2019-06-22 12:39:09 +0300

    Add API data transforming

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -16,28 +16,83 @@ export default class SwapiService {
 
   async getAllPeople() {
     const res = await this.getResource('/people/');
-    return res.results;
+    return res.results.map(SwapiService.transformPerson);
   }
 
-  getPerson(id) {
-    return this.getResource(`/people/${id}`);
+  async getPerson(id) {
+    const person = await this.getResource(`/people/${id}`);
+    return SwapiService.transformPerson(person);
   }
 
   async getAllPlanets() {
     const res = await this.getResource('/planets/');
-    return res.results;
+    return res.results.map(SwapiService.transformPlanet);
   }
 
-  getPlanet(id) {
-    return this.getResource(`/planets/${id}`);
+  async getPlanet(id) {
+    const planet = await this.getResource(`/planets/${id}`);
+    return SwapiService.transformPlanet(planet);
   }
 
   async getAllStarships() {
     const res = await this.getResource('/starships/');
-    return res.results;
+    return res.results.map(SwapiService.transformStarship);
   }
 
-  getStarship(id) {
-    return this.getResource(`/starships/${id}`);
+  async getStarship(id) {
+    const starship = await this.getResource(`/starships/${id}`);
+    return SwapiService.transformStarship(starship);
+  }
+
+  static extractId(item) {
+    const idRegExp = /\/([0-9]*)\/$/;
+    const [, id] = item.url.match(idRegExp);
+    return id;
+  }
+
+  static transformPerson(person) {
+    const {
+      name, gender, birthYear, eyeColor,
+    } = person;
+
+    return {
+      id: SwapiService.extractId(person),
+      name,
+      gender,
+      birthYear,
+      eyeColor,
+    };
+  }
+
+  static transformPlanet(planet) {
+    const {
+      name, population, rotation_period: rotationPeriod, diameter,
+    } = planet;
+
+    return {
+      id: SwapiService.extractId(planet),
+      name,
+      population,
+      rotationPeriod,
+      diameter,
+    };
+  }
+
+  static transformStarship(starship) {
+    const {
+      name, model, manufacturer, costInCredits, length, crew, passengers, cargoCapacity,
+    } = starship;
+
+    return {
+      id: SwapiService.extractId(starship),
+      name,
+      model,
+      manufacturer,
+      costInCredits,
+      length,
+      crew,
+      passengers,
+      cargoCapacity,
+    };
   }
 }
