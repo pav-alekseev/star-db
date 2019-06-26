d2e0722bd407de9f88ae77916ed8345022fe4501
RukkiesMan <pavelekseev@gmail.com>
2019-06-26 11:22:21 +0300

    Add error catching to the App component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,11 +3,32 @@ import React, { Component } from 'react';
 import Header from '../header';
 import RandomPlanet from '../random-planet';
 import PeoplePage from '../people-page';
+import ErrorIndicator from '../error-indicator';
 
 import './app.css';
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  componentDidCatch() {
+    this.setState({
+      hasError: true,
+    });
+  }
+
   render() {
+    const { hasError } = this.state;
+
+    if (hasError) {
+      return <ErrorIndicator />;
+    }
+
     return (
       <div>
         <Header />
