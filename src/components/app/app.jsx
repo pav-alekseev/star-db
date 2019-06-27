94fbae7724d311b08e54fda6405ee94417276e34
RukkiesMan <pavelekseev@gmail.com>
2019-06-27 20:08:44 +0300

    Add routers for the object pages

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import { PeoplePage } from '../pages';
+import {
+  PeoplePage,
+  PlanetsPage,
+  StarshipsPage,
+} from '../pages';
 import ErrorBoundry from '../error-boundry';
 import ErrorIndicator from '../error-indicator';
 import SwapiService from '../../services/swapi-service';
 import { SwapiServiceProvider } from '../swapi-service-context';
 
 import './app.css';
+import { StarshipDetails } from '../sw-components';
 
 export default class App extends Component {
   constructor() {
@@ -36,16 +42,21 @@ export default class App extends Component {
     return (
       <ErrorBoundry>
         <SwapiServiceProvider value={this.swapiService}>
-          <div>
-            <Header />
-            <div className="container">
-              <div className="my-3">
-                <RandomPlanet />
+          <Router>
+            <div>
+              <Header />
+              <div className="container">
+                <div className="my-3">
+                  <RandomPlanet />
+                </div>
+                
+                <Route path="/" render={() => <h2>Welcome to StarDB</h2>} exact />
+                <Route path="/people" component={PeoplePage} />
+                <Route path="/planets" component={PlanetsPage} />
+                <Route path="/starships" component={StarshipsPage} exact />
               </div>
-
-              <PeoplePage />
             </div>
-          </div>
+          </Router>
         </SwapiServiceProvider>
       </ErrorBoundry>
     );
